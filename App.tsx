38310e2057d0676686b2e77f742075e1e7bae2e8
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-26 09:03:08 -0300

    Used styled-components provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
+
 import { Catalog } from "./src/screens/Catalog";
+import { Product } from "./src/screens/Product";
+
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="dark" translucent backgroundColor="transparent" />
-      <Catalog />
-    </>
+      <Product />
+    </ThemeProvider>
   );
 }
