389ce189a61e7dee4d36233a603f1db03074739c
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-26 17:44:22 -0300

    Added ProductFullContent component

--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -2,20 +2,13 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors.background.secondary};
+  background-color: ${({ theme }) => theme.colors.background.tertiary};
 `;
 
 export const Content = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center;
-  background-color: ${({ theme }) => theme.colors.background.primary};
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.background.tertiary};
   z-index: -1;
-`;
-
-export const Text = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.family.bold};
-  font-size: ${({ theme }) => theme.fonts.size.xxl};
-  line-height: ${({ theme }) => theme.fonts.lineHeight.xl};
-  color: ${({ theme }) => theme.colors.text.primary};
 `;
