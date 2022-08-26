2acf8b233297476a59447bb13cacddd528bb0988
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-26 09:46:44 -0300

    Configured font family

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 
-import { Catalog } from "./src/screens/Catalog";
+import {
+  useFonts,
+  Lato_700Bold,
+  Lato_400Regular,
+} from "@expo-google-fonts/lato";
+
 import { Product } from "./src/screens/Product";
 
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_700Bold,
+    Lato_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="dark" translucent backgroundColor="transparent" />
