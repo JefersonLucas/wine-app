78e34e45e834858ba1398003ef51b63bde6342d2
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-26 17:39:44 -0300

    Getting more props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,14 @@ import { HeaderProps } from "./types";
 import { Logo } from "../Logo";
 import { Cart } from "../Cart";
 
-export const Header = ({ isLogo, isBackground, totalItems }: HeaderProps) => {
+export const Header = ({
+  isLogo,
+  isBackground,
+  totalItems,
+  ...rest
+}: HeaderProps) => {
   return (
-    <Container isBackground={isBackground}>
+    <Container isBackground={isBackground} {...rest}>
       {isLogo && <Logo />}
       <CartContainer>
         <Cart totalItems={totalItems} />
--- src/components/Header/types.d.ts
+++ src/components/Header/types.d.ts
@@ -2,4 +2,5 @@ export type HeaderProps = {
   isLogo?: boolean;
   isBackground?: boolean;
   totalItems: number;
+  elevation?: number;
 };
