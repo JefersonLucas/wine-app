70b05ef0bbb132effcad35df505e8fb482948eb5
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-26 13:43:51 -0300

    Created Logo component

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+import wineLogo from "../../assets/img/wine-logo.png";
+
+export const Container = styled.Image.attrs({
+  source: wineLogo,
+})`
+  width: 78px;
+  height: 22px;
+`;
