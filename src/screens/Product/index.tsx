11e28808c2d2644bdf22819c28f6d7c8de32aead
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-27 14:30:51 -0300

    Getting parameter data

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -2,10 +2,18 @@ import { Header } from "../../components/Header";
 import { ProductFullContent } from "../../components/Products/ProductFullContent";
 import { Container, Content } from "./styles";
 
+import { useRoute } from "@react-navigation/native";
+
+type ParamsProps = {
+  id: number;
+};
+
 export function Product() {
+  const route = useRoute();
+  const { id } = route.params as ParamsProps;
   return (
     <Container>
-      <Header totalItems={0} />
+      <Header totalItems={id} />
       <Content>
         <ProductFullContent />
       </Content>
