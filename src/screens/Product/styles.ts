d48a002753239aa980816b7c51b5353d0d283b6a
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-29 11:51:34 -0300

    Created LoadContainer stylized component

--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -12,3 +12,10 @@ export const Content = styled.View`
   background-color: ${({ theme }) => theme.colors.background.tertiary};
   z-index: -1;
 `;
+
+export const LoadContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  align-self: center;
+  justify-content: center;
+`;
